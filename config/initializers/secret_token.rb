c6430bbeacbab80ab31605d47b94d6d58f2efdcf
Randy Morgan <digital.ipseity@gmail.com>
2012-06-19 15:51:29 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rekk::Application.config.secret_token = '3b2210a1b1c89f7a4c9abdc3626112f6979880d44c8d3d37030551103cf2ee76cb4f6e96a43fee2c4a23839a955b3db38c941bd096dc3915cf3aedea2a90c71d'
