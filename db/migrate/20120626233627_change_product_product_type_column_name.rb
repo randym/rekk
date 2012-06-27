ce98113438fd95271dfd4f4e2a42061f91a71888
Randy Morgan <digital.ipseity@gmail.com>
2012-06-27 10:41:28 +0900

    fix table name in migration

--- db/migrate/20120626233627_change_product_product_type_column_name.rb
+++ db/migrate/20120626233627_change_product_product_type_column_name.rb
@@ -1,6 +1,6 @@
 class ChangeProductProductTypeColumnName < ActiveRecord::Migration
   def change
-    change_table :clients do |t|
+    change_table :products do |t|
       t.rename :product_type, :product_type_id
     end
   end
